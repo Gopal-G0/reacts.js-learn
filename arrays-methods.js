829a355bf2c61e84e0fc97b919430f0310d17cff
Gopal-G0 <heyitsmegopal@gmail.com>
2026-02-23 08:11:42 +0530

    learn array methods

--- arrays-methods.js
+++ arrays-methods.js
@@ -4,17 +4,22 @@
 //map 
 const userInfo = [
   {
-    name: "user1",
-    age: 20,
+    name: "Hitler",
+    age: 54,
     Country: "Germany"
   },
   {
-    name: "user2",
-    age: 25,
+    name: "Xi Jinpin",
+    age: 75,
     Country: "China"
   },
   {
-    name: "user3",
+    name: "Arthur Chen",
+    age: 29,
+    Country: "China"
+  },
+  {
+    name: "Lee Byung Hun",
     age: 28,
     Country: "South Korea"
   }
@@ -31,3 +36,48 @@ let countries = userInfo.map((user) => {
 
 console.log(userNames);
 console.log(countries);
+
+//find method --> it searches the array of obj, and it just returns the very first occurence of given conditions
+// it will ignore all other occurences
+// SO, it must be used only in the case of UNIQUE IDENTIFICATION
+
+let userFromChina = userInfo.find((user, index) => {
+  return user.Country === 'China';
+});
+
+console.log(userFromChina);
+
+let usersFromChina = userInfo.filter((user) => {
+  return user.Country === 'China';
+});
+
+console.log(usersFromChina);
+
+//'some' method --> it checks a condition, if any element satisfies it, it returns true otherwise returns false.
+
+
+let checkCondition = userInfo.some((user) => {
+  return user.age > 80;
+});
+
+console.log(checkCondition);
+
+// while 'every' method --> it checks the given condition on every element, if every element 
+// satisfies the condition then only it returns true else false.
+
+let checkForAll = userInfo.every((user) => {
+  return user.age > 25;
+});
+
+console.log(checkForAll);
+
+//includes(element, fromIndex) --> checks if given element exists in array or not
+
+const fruits = ["Mangoes", "Guava", "Pomegranate"];
+
+console.log(fruits.includes("Mangoes"));
+
+//indexOf() --> returns the index of the first occurence of element given in the array
+// returns -1 if element not found
+
+console.log(fruits.indexOf)
