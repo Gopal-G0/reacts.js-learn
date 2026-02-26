11b98cafbbb37a21f7544fe43590aa498937a80c
Gopal-G0 <heyitsmegopal@gmail.com>
2026-02-26 09:39:12 +0530

    made input & output section with input field and a button

--- react.js/todo-app/src/App.jsx
+++ react.js/todo-app/src/App.jsx
@@ -1,8 +1,65 @@
 export default function App() {
   
   return (
-    <div>
-      Minimalistic Task-Manager-App
+    <div
+      style={{
+        display: "flex",
+        border: "2px solid grey",
+        height: "90vh",
+        margin: 15,
+        borderRadius: 15,
+        padding:20
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 10,
+          alignItems: "center",
+          justifyContent: "center",
+          width: "65vw",
+          height: "90vh",
+          border: "1px solid grey",
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter your task to add"
+          required={true}
+          style={{
+            width: 450,
+            height: 50,
+            borderRadius: 9,
+            fontSize: 16,
+            paddingLeft: 15
+          }}
+        />
+        
+        <button
+          style={{
+            width: 120,
+            height: 45,
+            borderRadius: 8,
+            border: "none",
+            fontSize: 18,
+            fontWeight: 550,
+            backgroundColor: "#AF37FF",
+            color: "#000000",
+          }}
+        >
+          Add Task
+        </button>
+      </div>
+      <div
+        style={{
+          width: "65vw",
+          height: "90vh",
+          border: "1px solid grey",
+        }}
+      >
+        Right Tasks Display Area
+      </div>
     </div>
   );
 }
